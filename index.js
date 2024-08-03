3b6a82dd2f41031eb8b37fbbf049226786007b84
Rameshadhikari1999 <rameshadhikari199917@gmail.com>
2024-08-03 22:03:54 +0530

    project start

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+
+app.get('/test', (req, res) => {
+    res.send('Hello Welcom user!')
+});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
